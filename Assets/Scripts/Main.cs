c4ce534d2f37737b2db8f99bfe4f966f4f957f13
ProgrammerD23 <nikitadergauz@gmail.com>
2022-09-12 16:19:38 +0300

    Update 6

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,16 +8,20 @@ namespace GeekbrainsPlatformer
     {
         [SerializeField] private InteractiveObjectView playerView;
         [SerializeField] private CannonView cannonView;
+        [SerializeField] private GenerateLevelView _generateLevelView;
 
         private MainHeroWalker hero;
         private CannonControler cannonControler;
         private EmiterControler emiterControler;
+        private GeneratorLevelController _generatorLevelController;
 
         void Awake()
         {
             hero = new MainHeroWalker(playerView);
             cannonControler = new CannonControler(cannonView.muzzleT, playerView.transform);
             emiterControler = new EmiterControler(cannonView.bullets, cannonView.emiterT);
+            _generatorLevelController = new GeneratorLevelController(_generateLevelView);
+            _generatorLevelController.Awake();
         }
 
         void Update()
